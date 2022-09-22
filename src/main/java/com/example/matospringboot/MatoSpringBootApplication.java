23ce88a340e31c714579d75c63e4c1a57919f10f
tomatoYum <tomato6726@gmail.com>
2022-09-23 07:46:54 +0900

    yml set ⚙️

---
+++ src/main/java/com/example/matospringboot/MatoSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.matospringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MatoSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MatoSpringBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/matospringboot/MatoSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.matospringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MatoSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
